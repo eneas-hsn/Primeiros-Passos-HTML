25a750d655a531356444422e590605bd14efd11d
eneas-hsn <eneashsn@gmail.com>
2020-04-15 16:31:03 -0300

    deu certo

--- Alguns Exercicios/exerciciodia/script.js
+++ Alguns Exercicios/exerciciodia/script.js
@@ -3,18 +3,22 @@ function carregar() {
 var hora= data.getHours()
 var msg= window.document.getElementById('msg')
 var img= window.document.getElementById('img')
+var res= document.getElementById('res')
 msg.innerHTML=  `Agora são ${hora} horas.`
 if (hora>0 && hora <=12) {
-    img.src= 'manha.png.jpg'
+    img.src= 'manha.jpg'
     document.body.style.backgroundColor='rgb(218, 255, 83)' 
+    res.innerHTML='Bom dia!'
 }
 else if (hora >12 && hora<18) {
-    img.src= 'tarde.png.jpg'
+    img.src= 'tarde.jpg'
     document.body.style.backgroundColor='rgb(216, 147, 43)'
+    res.innerHTML='Boa tarde!'
 }
 else if (hora>18 && hora<=23) {
-    img.src= 'noite.png.jpg'
+    img.src= 'noite.jpg'
     document.body.style.backgroundColor='rgb(54, 44, 29)'
+    res.innerHTML='Boa noite!'
 }
 
 }
